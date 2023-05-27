991cb3bf52eaaccb60a1a74fb4d8c66bbf3f95f9
Andrea Pasquale <andrea.pasquale@outlook.it>
2023-05-27 18:08:05 +0200

    added logo

--- docs/make.jl
+++ docs/make.jl
@@ -5,7 +5,7 @@ const CI = get(ENV, "CI", "false") == "true"
 
 makedocs(;
     authors="Andrea Pasquale <andrea.pasquale@polimi.it> and Michele Ceresoli <michele.ceresoli@polimi.it>",
-    sitename="JSMDocs.jl",
+    sitename="Julia Space Mission Design",
     modules=[JSMDocs],
     format=Documenter.HTML(; prettyurls=CI, highlights=["yaml"], ansicolor=true),
     pages=[
