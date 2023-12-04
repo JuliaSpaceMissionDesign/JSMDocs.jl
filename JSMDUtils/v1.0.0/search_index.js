da4465b59b8650ffce5e82773f2681967e4dd102
Andrea Pasquale <andrea.pasquale@polimi.it>
2023-12-04 22:36:22 +0100

    reset

--- JSMDUtils/v1.0.0/search_index.js
+++
@@ -1,3 +0,0 @@
-var documenterSearchIndex = {"docs":
-[{"location":"#JSMDUtils.jl","page":"Home","title":"JSMDUtils.jl","text":"","category":"section"},{"location":"utils/#Utilities","page":"Utilities","title":"Utilities","text":"","category":"section"}]
-}
